8e2b68cbfe662e31caf38acc04f50717040dcec6
Guilherme Queiroz <queirozguilherme05@gmail.com>
2024-03-20 22:43:59 -0300

    nova

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def teste():
 @app.route("/telegram", methods=["POST"])
 def telegram_update():
   update= request.json
-  url_envio_mensagem=f"https://api.telegram.org.bot{BOT_TOKEN}/sendMessage"
+  url_envio_mensagem=f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
   chat_id= update["message"]["chat"]["id"]
   mensagem= {"chat_id": chat_id, "text": "mensagem <b>recebida</b>!", "parse_mode": "HTML"}
   requests.post(url_envio_mensagem, data=mensagem)
